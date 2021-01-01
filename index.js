47d0af10951564b96a43f159f4afc9e0b05eee93
ZacharyFolk <zacharyfolk@gmail.com>
2021-01-01 14:05:51 -0800

    Example mututation with fake data

--- index.js
+++ index.js
@@ -8,7 +8,48 @@ app.get('/', (req, res) => {
     res.send('Sending a response!');
 });
 
-const root = { hello: () => "Hi"};
+// fake db for now
+
+const friendDatabase = {};
+
+class friend {
+    constructor(id, { firstName, lastName, gender, email}){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+    }
+}
+
+
+const root = { 
+    friend: () => {
+        return {
+            "id" : 3433434343,
+            "firstName": "Zac",
+            "lastName": "Folk",
+            "gender": "Male",
+            "email": "zacharyfolk@gmail.com"
+        }
+    },
+
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new friend(id, input);
+    },
+    
+    exercise: () => {
+        return {
+            "id": 1234,
+            "name" : "pushups",
+            "reps" : 3,
+            "weight" : 30
+
+        }
+    }
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
--- schema.js
+++ schema.js
@@ -1,10 +1,51 @@
 import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
-type Query {
-    hello: String
+type Friend {
+     id: ID
+     firstName: String
+     lastName: String
+     gender: String
+     email: String
 }
 
+type Email {
+    email: String
+}
+
+type Exercise {
+    id: ID
+    name: String!
+    reps: Int
+    weight: Int
+    duration: Int
+}
+
+type Workout {
+    id: ID
+    name: String
+    exercises: [Exercise]!
+    date: Date
+}
+
+type Query {
+    friend: Friend
+    exercise: Exercise
+}
+
+input FriendInput {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: String
+        email: String
+}
+
+type Mutation {
+    createFriend(input: FriendInput): Friend
+}
+
+scalar  Date
 `)
 
 export default schema;
